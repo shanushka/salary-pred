90a7246257f703dc530b98c8dcd848fdc1613a29
fazkhan123 <faraazk3272@gmail.com>
2023-04-10 13:45:30 -0500

    New changes.

--- Faraaz/concatenate.py
+++ Faraaz/concatenate.py
@@ -1,20 +1,16 @@
 import pandas as pd
 
 
-df1 = pd.read_csv('Indeed_JD_electricaldesignngineer.csv')
-df2 = pd.read_csv('Indeed_JD_highvoltageengineer.csv')
-df3 = pd.read_csv('Indeed_JD_powerelectronics.csv')
-df4 = pd.read_csv('Indeed_JD_signalprocessing.csv')
-df5 = pd.read_csv('Indeed_JD_systemembeddedengineer.csv')
-df6 = pd.read_csv('Indeed_JD_transmissionlineengineer.csv')
-df = pd.concat([df1, df2, df3, df4, df5, df6])
+df1 = pd.read_csv('Indeed_JD_Attorney.csv')
+df2 = pd.read_csv('Indeed_JD_LawClerk.csv')
+df3 = pd.read_csv('Indeed_JD_Paralegal.csv')
 
 
-#df["JobType"] = "Banking"
-#df["Industry"] = "Finance"
+df = pd.concat([df1, df2, df3 ])
+#df["Industry"] = "Human Resources"
 
 #df_1  =pd.read_csv('RawData.csv')
 
 #s = pd.concat([df, df_1])
 
-df.to_csv("Electricalengineer.csv")
+df.to_csv("Data science.csv")
--- Faraaz/join.py
+++ Faraaz/join.py
@@ -1,24 +1,29 @@
 import pandas as pd
 
 
-df = pd.read_csv("Indeed_JD_renewableengineer.csv")
-#df2 = pd.read_csv("Indeed_JD_highvoltageengineer.csv")
+df1 = pd.read_csv("Indeed_JD_PersonalTrainer.csv")
+df2 = pd.read_csv("Indeed_JD_Referee.csv")
+df3 = pd.read_csv("Indeed_JD_SportsCoach.csv")
+#df4 = pd.read_csv("Indeed_JD_HotelManager.csv")
+#df5 = pd.read_csv("Indeed_JD_TravelAgent.csv")
+#df6 = pd.read_csv("Indeed_JD_Photographer.csv")
+#df7 = pd.read_csv("Indeed_JD_TatooArtist.csv")
+
+df1["JobType"] = "PersonalTrainer"
+df2["JobType"] = "Referee"
+df3["JobType"] = "SportsCoach"
+#df4["JobType"] = "Hotel Manager"
+#df5["JobType"] = "Travel Agent"
+#df6["JobType"] = "Photographer"
+#df7["JobType"] = "Tatoo Artist"
+
+df = pd.concat([df1, df2, df3 ])
 
 
-
-df["JobType"] = "renewableengineer"
-#df2["JobType"] = "highvoltageengineer"
-
-
-
-
-#df = pd.concat([df1, df2, df3, df4, df5, df6])
-
-
-df["Industry"] = "renewable engineering"
+df["Industry"] = "Sport and Recreation"
 
 #df_1  =pd.read_csv('RawData.csv')
 
 #s = pd.concat([df, df_1])
 
-df.to_csv("renewable engineering.csv")
+df.to_csv("SportsandRecreation.csv")
